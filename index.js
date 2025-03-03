dd51db734ad06b94bbd8534e05834e4c20af88c9
Pret Secretariat <pretsecretariat@administration.paris-sorbonne.fr>
2025-03-03 12:09:35 +0100

    test of chalk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import chalk from "chalk";
 
 const colleagues = [
     chalk.redBright('Romi'),
-    chalk.bgGray('Elaine'),
+    chalk.bgBlack('Elaine'),
     chalk.blueBright('Patrick'),
     chalk.bgMagentaBright('Anne')
 ];
