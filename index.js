24e5dc23056d2463f28cc07ad4aa30d8aab2505a
Pret Secretariat <pretsecretariat@administration.paris-sorbonne.fr>
2025-03-03 14:45:13 +0100

    exo-fs

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-import chalk from "chalk";
+import fs from 'fs/promises';
+import http from "http"
 
-const colleagues = [
-    chalk.redBright('Romi'),
-    chalk.bgBlack('Elaine'),
-    chalk.blueBright('Patrick'),
-    chalk.bgMagentaBright('Anne')
-];
+const PORT = 8000
+const localhost = 'localhost'
+const file='test.html'
+const content= 'test'
 
-colleagues.forEach(name => console.log(name));
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello World</h1>');
+   });
+   server.listen(PORT, localhost, () => {
+    console.log(`Server is running on port ${PORT}`);
+   })
+const writeAndRead=async ()=>{
+try{
+    await fs.writeFile(file, content)
+    const data = await fs.readFile(file, 'utf8');
+  console.log(data);
+    }catch (err) {
+    console.log(err);
+  }
+}
+
+writeAndRead()
